ca1db4eb55f2520b19840fbfc518edd37186e075
Nyi <nyihtet4dev@icloud.com>
2026-06-19 10:10:21 -0700

    Initial Commit

---
+++ PureConstruct AI/PureConstruct_AIApp.swift
@@ -0,0 +1,32 @@
+//
+//  PureConstruct_AIApp.swift
+//  PureConstruct AI
+//
+//  Created by Nyi on 6/19/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PureConstruct_AIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
